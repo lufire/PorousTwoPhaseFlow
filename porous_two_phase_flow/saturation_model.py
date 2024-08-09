f0d5ff7103fac73f62e83a9bdaa470199de42bd0
lufire <lukas.feierabend@gmail.com>
2024-08-09 14:44:54 +0200

    shape edit

--- porous_two_phase_flow/saturation_model.py
+++ porous_two_phase_flow/saturation_model.py
@@ -425,11 +425,11 @@ class ImbibitionDrainageCurve(SaturationModel):
 
     def calc_capillary_pressure(self, saturation, capillary_pressure_prev=None,
                                 **kwargs):
-        if isinstance(saturation, np.ndarray):
-            shape = saturation.shape
-            saturation[saturation < self.s_min] = self.s_min
-            saturation[saturation > 1.0] = 1.0
-            saturation = saturation.ravel(order='F')
+        saturation = np.copy(np.asarray(saturation))
+        shape = saturation.shape
+        saturation[saturation < self.s_min] = self.s_min
+        saturation[saturation > 1.0] = 1.0
+        saturation = saturation.ravel(order='F')
 
         def root_saturation(pressure):
             return saturation - \
